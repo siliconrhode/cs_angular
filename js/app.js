4415413c9b617504d0b3b27680654a4a948eced3
Silicon Rhoder <siliconrhode@gmail.com>
2017-10-09 17:39:07 -0400

    different solution

--- js/app.js
+++ js/app.js
@@ -5,27 +5,33 @@ var app = angular.module('store', []);
 
 app.controller('StoreController', function(){
 
-	this.products = gems;
+	this.gems =  [
+	{
+
+		name:"Free People", 
+		price: 99.99,
+		description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. .......",
+		canPurchase:true,
+		soldOut: false
+	},
+
+	{
+		name:"Tommy Hilfiger",
+		price: 40.00,
+		description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+		canPurchase:true,
+		soldOut:false
+	}
+
+	];
+
+
+
+
+
+
 });
 
-var gems = [
-{
 
-	name:"Free People", 
-	price: 99.99,
-	description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. .......",
-	canPurchase:true,
-	soldOut: false
-},
-
-{
-	name:"Tommy Hilfiger",
-	price: 40.00,
-	description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-	canPurchase:true,
-	soldOut:false
-}
-
-];
 
 })();
