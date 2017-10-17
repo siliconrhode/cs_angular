efacbde6d58d298bed03554314137d2d4343d9dd
Silicon Rhoder <rhoder@Sams-MacBook-Pro.local>
2017-10-17 18:10:53 -0400

    add panel controller

--- js/app.js
+++ js/app.js
@@ -3,33 +3,19 @@ var app = angular.module('store', []);
 
 
 
-app.controller('StoreController', function(){
+	
+	app.controller('PanelController', function(){
+		this.tab = 1;
 
-	this.products = gems;  
-
-
-
-});
-gems=[
+		this.selectTab = function(tabNo) {
+			this.tab = tabNo;
+		};
 		
-		{
+		this.isSelected = function(checkNo){
+			return this.tab === checkNo
+		};
+	});
 
-			name:"Free People", 
-			price: 99.99,
-			description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. .......",
-			canPurchase:true,
-			soldOut: false
-		},
-
-		{
-			name:"Tommy Hilfiger",
-			price: 40.00,
-			description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-			canPurchase:true,
-			soldOut:false
-		}
-
-	];
 
 
 })();
