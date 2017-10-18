9c9797f651c278fc3fefd5ade76fe1d97a55c91a
Silicon Rhoder <rhoder@Sams-MacBook-Pro.local>
2017-10-17 20:28:17 -0400

    add product specs and images

--- js/app.js
+++ js/app.js
@@ -1,21 +1,58 @@
 (function(){
 var app = angular.module('store', []);
 
+app.controller('StoreController',function(){
+	this.products = gems;
+});
 
 
+app.controller('PanelController', function(){
+	this.tab = 1;
+
+	this.selectTab = function(tabNo) {
+		this.tab = tabNo;
+	};
 	
-	app.controller('PanelController', function(){
-		this.tab = 1;
+	this.isSelected = function(checkNo){
+		return this.tab === checkNo
+	};
+});
 
-		this.selectTab = function(tabNo) {
-			this.tab = tabNo;
-		};
-		
-		this.isSelected = function(checkNo){
-			return this.tab === checkNo
-		};
-	});
-
-
+var gems = [{
+    name: 'Azurite',
+    description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
+    shine: 8,
+    price: 110.50,
+    rarity: 7,
+    color: '#CCC',
+    faces: 14,
+    images: [ ]
+  }, {
+    name: 'Bloodstone',
+    description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
+    shine: 9,
+    price: 22.90,
+    rarity: 6,
+    color: '#EEE',
+    faces: 12,
+    images: [
+      "images/gem-01.gif",
+      "images/gem-03.gif",
+      "images/gem-04.gif"
+    ]
+  }, {
+    name: 'Zircon',
+    description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
+    shine: 70,
+    price: 1100,
+    rarity: 2,
+    color: '#000',
+    faces: 6,
+    images: [
+      "images/gem-06.gif",
+      "images/gem-07.gif",
+      "images/gem-09.gif"
+    ]
+  }];
 
 })();
